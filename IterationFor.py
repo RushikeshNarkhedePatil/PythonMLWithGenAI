250519e0d9d459490c21209df588b554efba73f8
RushikeshNarkhedePatil <rushikeshnarkhede4@gmail.com>
2026-01-11 07:50:48 +0530

    Program based on selection and used Lambda function

---
+++ IterationFor.py
@@ -0,0 +1,6 @@
+#Static Sequence
+print("1")
+print("2")
+print("3")
+print("4")
+print("5")
---
+++ IterationForXX.py
@@ -0,0 +1,10 @@
+# Sequence
+print("Jay Ganesh")
+print("Jay Ganesh")
+print("Jay Ganesh")
+print("Jay Ganesh")
+
+print("_"*32)
+# Iteration
+for i in range(4):
+    print("Jay Ganesh")
